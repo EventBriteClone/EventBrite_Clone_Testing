bc81f58657f1d2df6ed8919d94c029937f778514
youssef-osama <youssef.mohamady00@eng-st.cu.edu.eg>
2023-03-25 17:41:29 +0200

    Update LikingTests.py

--- Web/LikingTests.py
+++ Web/LikingTests.py
@@ -8,16 +8,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import datetime
+import WebsiteDriverInit
 
 mainURL="https://www.eventbrite.com/"
-path = r"C:\Users\youss\Desktop\SWE\msedgedriver.exe"
 loginButtonXPATH = "/html/body/div[2]/div/div[1]/div/div/header/div/div[1]/div[2]/div[2]/a[1]/span[2]"
-
-driver = webdriver.Edge()
+driver = WebsiteDriverInit.init()
 driver.get(mainURL)
-
 driver.maximize_window()
-# driver = WebsiteDriverInit.init()
 usernameT=" miraaayman770@gmail.com"
 passwordT="Software123?"
 login = WebDriverWait(driver, 10).until(lambda x: x.find_element(By.XPATH, loginButtonXPATH))
@@ -67,55 +64,3 @@ sleep(10)
 assert "Add events, share with friends!" in driver.page_source
 driver.close()
 ###########
-
-# driver.find_element(By.XPATH,"/html/body/div[1]/div/div[2]/div/div/div/div[1]/div/main/section/div/main/div/main/div[1]/div/div/div/div/div/div/article/div[3]/div/span[2]/span/button/i/svg/path").click()
-# sleep(10)
-# input=driver.find_element(By.XPATH,"/html/body/div[2]/div/div[2]/div/div/div/div[1]/div/main/section[2]/div/div/div[1]/div[2]/div/div/div/div[1]/div/div/div/input")
-# input.send_keys("Cairo")
-# input.send_keys(Keys.RETURN)
-# input.send_keys(Keys.ENTER)
-# driver.find_element(By.XPATH,"/html/body/div[2]/div/div[2]/div/div/div/div[1]/div/main/section[2]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div/ul/li[1]/div/button/div/div/div[1]").click()
-# sleep(20)
-# driver.find_element(By.XPATH,"/html/body/div[2]/div/div[2]/div/div/div/div[1]/div/main/section[2]/div/div/div[2]/div/div/div/div/div/section[1]/div/div/div[2]/div[1]/div/div/article/div[1]/div[3]/span/span/button/i/svg").click()
-
-# search_box=driver.find_element(By.XPATH,"/html/body/div[2]/div/div[1]/div/div/header/div/div[1]/div[1]/button/div/div/div/div")
-# search_box.click()
-
-# sleep(10)
-# # search_box=driver.find_element(By.ID,"global-header")
-# # search_box.click()
-
-# sleep(10)
-# driver.find_element(By.XPATH,"/html/body/div[2]/div[2]/div/div/div/div/div[1]/div/div/main/div/div/div/main/header/div/ul/li[2]/div/span").click()
-# sleep(10)
-# search_box=driver.find_element(By.XPATH,"/html/body/div[2]/div/div[2]/div/div/div/div[1]/div/main/div/div/section[1]/div/header/form/nav/div[1]/div[1]/div[2]/div/div/div/div[1]/div/div/div/input")
-# sleep(10)
-# search_box.send_keys("Egypt")
-# search_box.send_keys(Keys.RETURN)
-# search_box.send_keys(Keys.ENTER)
-# driver.find_element(By.XPATH,"/html/body/div[2]/div/div[2]/div/div/div/div[1]/div/main/div/div/section[1]/div/header/form/nav/div[1]/div[1]/div[2]/div/div/div/div[2]/div/div/ul/li[1]/div/button/div/div/div[1]").click()
-# sleep(5)
-# page_source = driver.page_source
-# word_count = page_source.count("Today")
-
-# # Print the word count
-# print(f"The word 'Today' appears {word_count} times on the webpage.")
-# search_results=(driver.find_elements(By.XPATH,"/html/body/div[2]/div/div[2]/div/div/div/div[1]/div/main/div/div/section[1]/div/section"))
-# sleep(10)
-# second=driver.find_element(By.XPATH,"/html/body/div[2]/div/div[2]/div/div/div/div[1]/div/main/div/div/section[1]/div/div/div/section/ul/li[1]/div/div[1]/div/div/div")
-# second.click()
-# # print("second",second)
-# print(second("Alice"))
-# text = second.text()
-# sleep(20)
-# # search for the word 'example' in the text
-# if 'Today' in text:
-#     print('The word "example" was found on the page!')
-# else:
-#     print('The word "example" was not found on the page.')
-
-# num_results = len(search_results)
-# print("Number of search results:", num_results)
-# print("search_results",search_results)
-
-# assert "Today" in driver.page_source
